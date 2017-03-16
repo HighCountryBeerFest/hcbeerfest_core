d5db1657b992cdaecf8b4343700af4305df6f092
Zach Seifts <zach.seifts@gmail.com>
2017-03-16 12:13:37 -0400

    Starting to add in more homepage config

--- src/Controller/Home.php
+++ src/Controller/Home.php
@@ -19,9 +19,18 @@ class Home extends ControllerBase {
    */
   public function index() {
 
+    $ticket_block_vip = array(
+      'on_sale' => \Drupal::state()->get('hcbeerfest_core_tickets_on_sale_vip') ?: 0,
+      'link' => \Drupal::state()->get('hcbeerfest_core_tickets_link_vip') ?: 0,
+      'price' => \Drupal::state()->get('hcbeerfest_core_tickets_price_vip') ?: 0,
+    );
+
     return array(
       '#theme' => 'hcbeerfest_home',
       '#release_party' => \Drupal::state()->get('hcbeerfest_core_tickets_release_party') ?: 0,
+      '#week_of_message' => \Drupal::state()->get('hcbeerfest_core_tickets_release_party') ?: 0,
+      '#tickets_on_sale' => \Drupal::state()->get('hcbeerfest_core_tickets_on_sale') ?: 0,
+      '#ticket_block_vip' => $ticket_block_vip,
     );
   }
 }
